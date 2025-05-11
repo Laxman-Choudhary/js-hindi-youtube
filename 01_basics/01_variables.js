b953e2d7860cbe1a9d111f9560ca706d0e1d5245
Laxman Choudhary <99634506+Laxman-Choudhary@users.noreply.github.com>
2025-05-11 09:09:44 +0000

    dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=14453;
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+// accountId=2 //not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="213443"
+accountCity="Bengluru"
+
+let accountState;
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using node js ,not browser
+
+console.log(3+3); //code  readbility shoud be higher
+console.log("Hitesh");
+
+let name ="hitesh"
+let age=15
+let isLoggedIn = false
+
+// number 2 ^53 
+// bigint
+// string ""
+// boolean => true/false
+// null=> standalone value
+// undefined
+// symbol unique
+
+// object
+
+console.log(typeof("hitesh"))
+console.log(typeof undefined);
+console.log(typeof(null))
