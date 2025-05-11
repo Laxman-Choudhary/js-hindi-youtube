876118a90512e0c4139337170990892dac6dd544
Laxman Choudhary <99634506+Laxman-Choudhary@users.noreply.github.com>
2025-05-11 09:56:15 +0000

    datatype summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1)
+
+// console.log("2">1)
+
+// console.log(num>0);
+// console.log(nul==0)
+
+
+// console.log(undefined==0);
+
+// ===
+console.log("2"===2);
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+// primitive data
+// 7
+// string number, boolean null undefined  symbol  BigInt
+
+// const score=100
+// const scoreValue=100.3
+
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail=undefined;
+
+const id=Symbol("123")
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+const bigNumber=323123452345114331n
+
+
+
+
+
+// non primitive
+// reference type
+
+// array, objects,  functions, 
+
+const heros=["shaktiman","naagraj","doga"]
+
+let myObj={
+    name:"hitesh",
+    age:22
+}
+
+const myFunction=function(){
+    console.log("hello world");
+}
+
